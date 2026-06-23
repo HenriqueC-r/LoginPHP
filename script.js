9a8bd40eb1cd75f94cc65d5612a7bb265392d93f
mirianrl <miriannrl08@gmail.com>
2026-06-23 19:38:32 -0400

    Estrutura do dashboard

--- script.js
+++ script.js
@@ -2,3 +2,27 @@ const usuario = document.getElementById("usuario");
 const senha = document.getElementById("senha");
 const botao = document.getElementById("entrar");
 
+// TABELA DE USUÁRIOS
+
+fetch("usuarios.php")
+.then(response => response.json())
+.then(usuarios => {
+
+    const tabela = document.getElementById("tabela-usuarios");
+
+    if (!tabela) return; // evita erro nas páginas que não têm tabela
+
+    tabela.innerHTML = "";
+
+    usuarios.forEach(usuario => {
+        tabela.innerHTML += `
+            <tr>
+                <td>${usuario.id}</td>
+                <td>${usuario.nome}</td>
+                <td>${usuario.senha}</td>
+                <td><button>Excluir</button></td>
+            </tr>
+        `;
+    });
+
+});
