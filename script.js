4fd696fa6c8fea2186e09a15889a29b3c589f712
mirianrl <miriannrl08@gmail.com>
2026-06-22 20:10:27 -0400

    adicionando html, css e javaScript

---
+++ script.js
@@ -0,0 +1,10 @@
+const usuario = document.getElementById("usuario");
+
+const senha = document.getElementById("senha");
+
+if (
+    usuario.value === "mirian" &&
+    senha.value === "1234"
+) {
+    alert("Login realizado!");
+}
