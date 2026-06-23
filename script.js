3563a6514a44e014bcc141ae5e8e17dd16886202
mirianrl <miriannrl08@gmail.com>
2026-06-22 20:50:24 -0400

    mudei html e javascript

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
 const usuario = document.getElementById("usuario");
-
 const senha = document.getElementById("senha");
-
-if (
-    usuario.value === "mirian" &&
-    senha.value === "1234"
-) {
-    alert("Login realizado!");
-}
+const botao = document.getElementById("entrar");
